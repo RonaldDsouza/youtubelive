15dcb367bd96e3ea726239b666c8f4e28d870b40
mxplayermovies <46235644+RonaldDsouza@users.noreply.github.com>
2024-10-21 03:21:06 +0530

    soory ok

--- pages/index.js
+++ pages/index.js
@@ -137,7 +137,7 @@ export default function HomePage() {
           name="twitter:image"
           content="https://youtubelive.vercel.app/og_image.jpg"
         />
-        <meta name="google-adsense-account" content="ca-pub-4821855388989115">
+        <meta name="google-adsense-account" content="ca-pub-4821855388989115" />
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: rankMathSchema }}
