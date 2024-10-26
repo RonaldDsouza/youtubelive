d54f68985a5adb06f2fd458cd05f52ce99de4b49
mxplayermovies <110382877+veigasjeff@users.noreply.github.com>
2024-10-26 19:12:45 +0530

    CHECK

--- pages/music/index.js
+++ pages/music/index.js
@@ -446,7 +446,7 @@ export default function HomePage({ articles }) {
       {/* <div className={youtubeStyles.container} > */}
       <header className={youtubeStyles.header}>
         <h1 className={youtubeStyles.logo}>
-        🎵 Music Section.🎵
+        Music Section. 🎵 🎼🎶 🎷 🎸🎹 🎺 🪕 🥁 
         </h1>
       </header>
 
